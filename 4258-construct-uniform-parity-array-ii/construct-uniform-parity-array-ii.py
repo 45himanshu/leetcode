92c8c4cc235c71169259b7c47875e4a2d35c9299
Himanshu Raj <himanshusingh6753@gmail.com>
2026-09-04 00:55:49 +0530

    Time: 23 ms (76.22%) | Memory: 35.5 MB (28.66%) - LeetSync

---
+++ 4258-construct-uniform-parity-array-ii/construct-uniform-parity-array-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        # Case 1: All numbers are already even
+        if all(x % 2 == 0 for x in nums1):
+            return True
+
+        # Case 2: Make everything odd
+        # The smallest number must be odd.
+        return min(nums1) % 2 == 1
